4af86ff8e17d0d8f7ac26d27e2e3d0ca619cb27d
acstash <acdevhub@gmail.com>
2021-08-01 16:46:49 +0200

    Guess!

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,10 +1,13 @@
+# Import random and with the randint function we can generate  
+# the random number, in this case between 1 and 20.
 import random
-secret_num = random.randint(1,20)
-
+secret_num = random.randint(1,20)  
+# We ask the user for his name.
 print('Hello, what is your name?')
 name = input()
 print('Hi ' + name + ', i am thinking of a number between 1 and 20.')
-
+# A for loop to iterate as many times as the user has opportunities to guess the number
+# Then a simple if conditional to give some hints to the user
 for num_guesses in range (1,7):
     print('Take a guess')
     guess = int(input())
@@ -14,6 +17,8 @@ for num_guesses in range (1,7):
         print('The number you guessed is too high!')
     else:
         break
+# When the user guess the right number or surpasses the guess limit the for loop break
+# With another if conditional we let the user know whether if he guessed the right number or not.
 if guess == secret_num:
     print('Good job! The number i was thinking of was ' + str(secret_num))
 else:
